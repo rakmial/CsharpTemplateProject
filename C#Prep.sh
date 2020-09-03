7e5f1d0529973eee5236fb50a17cc5a1398a3f35
Joshua Lovelace <rakmial@gmail.com>
2020-09-03 10:49:26 -0700

    adds C#Prep.sh, bash script executable in .. above Template to copy to new dir and file names, inc references in files

---
+++ C#Prep.sh
@@ -0,0 +1,11 @@
+cp -a TemplateProject.Solution/ $1.Solution
+
+rename $1.Solution/TemplateProject $1.Solution/$1 $1.Solution/TemplateProject*
+
+mv ./$1.Solution/$1/TemplateProject.csproj ./$1.Solution/$1/$1.csproj
+mv ./$1.Solution/$1/Models/TemplateClass.cs ./$1.Solution/$1/Models/$1.cs
+mv ./$1.Solution/$1.Tests/TemplateProject.Tests.csproj ./$1.Solution/$1.Tests/$1.Tests.csproj
+mv ./$1.Solution/$1.Tests/ModelTests/TemplateClassTest.cs ./$1.Solution/$1.Tests/ModelTests/$1Test.cs
+
+sed -i s/TemplateClass/$1/g ./$1.Solution/$1{/Models/$1.cs,.Tests/{$1.Tests.csproj,ModelTests/$1Test.cs}}
+sed -i s/TemplateProject/$1/g ./$1.Solution/$1{/Models/$1.cs,.Tests/{$1.Tests.csproj,ModelTests/$1Test.cs}}
